a4d401ded7bb6cf21688f793b2d77f9d614b33ea
maugallo <maugallok@gmail.com>
2024-10-03 20:36:37 -0300

    Reorganización html y mejoras con signals

--- src/app/pages/pricing/pricing-meat/pricing-meat.component.ts
+++ src/app/pages/pricing/pricing-meat/pricing-meat.component.ts
@@ -4,10 +4,11 @@ import { IonContent, IonInput, IonText, IonAccordion, IonItem, IonLabel, IonAcco
 import { ProductService } from 'src/app/core/services/product.service';
 import { FormsModule, NgForm } from '@angular/forms';
 import { HALF_CARCASS_WEIGHT_AVERAGE, WEIGHT_AVERAGES } from 'src/app/core/constants/weight-average.constant';
-import { UpperCasePipe } from '@angular/common';
+import { NgFor, UpperCasePipe } from '@angular/common';
 import { ValidationService } from 'src/app/core/services/utils/validation.service';
 import { MinValueDirective } from 'src/app/shared/validators/min-value.directive';
 import { MaxValueDirective } from 'src/app/shared/validators/max-value.directive';
+import { AlertService } from 'src/app/core/services/utils/alert.service';
 
 @Component({
   selector: 'app-pricing-meat',
@@ -20,43 +21,60 @@ export class PricingMeatComponent {
 
   public validationService = inject(ValidationService);
   private productsService = inject(ProductService);
+  private alertService = inject(AlertService);
   
   public weightAverages = WEIGHT_AVERAGES;
   public halfCarcassWeightAverage = HALF_CARCASS_WEIGHT_AVERAGE;
   
-  public products = this.productsService.getProductsByCategory('carnes');
+  public products = computed(() => signal(this.productsOnlyReadSignal())); // WritableSignal<Signal> para poder actualizar el array.
+  private productsOnlyReadSignal = this.productsService.getProductsByCategory('carnes');
   public rawProducts = this.productsService.getProductsByCategory('carnes');
   
   public halfCarcassCost = signal('');
   public profitPercentage = signal('');
 
-  public totalSellingPrice = computed(() => {
-    return Number(this.halfCarcassCost()) * (Number(this.profitPercentage())/100);
-  });
+  public totalSellingPrice = computed(() => Number(this.halfCarcassCost()) * (Number(this.profitPercentage())/100));
+  public totalProductSum = computed(() => this.products()().reduce((acumulatedSum, product) => acumulatedSum + Number(product.price), 0));
 
-  public totalProductSum = computed(() => {
-    console.log("Los precios cambiaron, sumando de nuevo...")
-    return this.products().reduce((acumulatedSum, product) => acumulatedSum + Number(product.price), 0)
-  });
-
-  public onSubmit(pricingForm: NgForm) {
-    if (!pricingForm.valid) {
-      pricingForm.form.markAllAsTouched();
+  public onCalculateSubmit(calculateForm: NgForm) {
+    if (!calculateForm.valid) {
+      calculateForm.form.markAllAsTouched();
       return;
     }
     this.calculatePrices();
   }
 
-  public calculatePrices() {
-    //this.products
+  public onApplySubmit(applyForm: NgForm) {
+    if (!applyForm.valid) {
+      applyForm.form.markAllAsTouched();
+      return;
+    }
+    this.applyNewPrices();
   }
 
-  public applyNewPrices() {
+  private calculatePrices() {
+    const productsWithCalculatedPrice = this.products()().map((product, index) => {
+      const productPercentage = WEIGHT_AVERAGES[index].value / HALF_CARCASS_WEIGHT_AVERAGE.value;
+      const productPrice = this.totalSellingPrice() * productPercentage;
+
+      return {
+        ...product,
+        price: productPrice.toFixed(2)
+      }
+    });
+
+    this.products().set(productsWithCalculatedPrice);
+  }
+
+  private applyNewPrices() {
     
   }
 
   public areThereChangesInPrices() { // Hacemos Number() ya que sino se pueden comprar en string '5400.0' con '5400' en el caso de que vuelva a poner el precio original.
-    return (this.rawProducts().some((product, index) => Number(product.price) !== Number(this.products()[index].price)));
+    if (this.rawProducts().length > 0 && this.products()().length > 0) {
+      return (this.rawProducts().some((product, index) => Number(product.price) !== Number(this.products()()[index].price)));
+    }
+    return false;
   }
 
 }
